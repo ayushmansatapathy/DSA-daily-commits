284cb79a445671cc01d01bcf0f4b0eaf6bc7b8a1
ayushmansatapathy <ayushmansatapathy10@gmail.com>
2026-03-22 13:01:41 +0530

    Initial commit

---
+++ DAY-8/2d_inserion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 3, cols = 3;
+    int arr[3][3];
+
+    cout << "Enter elements:\n";
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> arr[i][j];   // insertion
+        }
+    }
+
+    cout << "2D Array:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
