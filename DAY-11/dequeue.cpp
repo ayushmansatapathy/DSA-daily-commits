2428155e22289d0f6ebaff069336eed169f4739a
ayushmansatapathy <ayushmansatapathy10@gmail.com>
2026-04-07 22:49:48 +0530

    dequeue operation

---
+++ DAY-11/dequeue.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 5
+
+int queue[SIZE];
+int front = -1, rear = -1;
+
+// Enqueue
+void enqueue(int value) {
+    if (rear == SIZE - 1) {
+        cout << "Queue Overflow\n";
+        return;
+    }
+
+    if (front == -1)
+        front = 0;
+
+    queue[++rear] = value;
+}
+
+
+void dequeue() {
+    if (front == -1 || front > rear) {
+        cout << "Queue Underflow\n";
+        return;
+    }
+
+    cout << "Deleted: " << queue[front] << endl;
+    front++;
+}
+
+// Display
+void display() {
+    if (front == -1 || front > rear) {
+        cout << "Queue is empty\n";
+        return;
+    }
+
+    cout << "Queue: ";
+    for (int i = front; i <= rear; i++) {
+        cout << queue[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+    display();
+
+    return 0;
+}
